f15242cf8949a56c2b42af3313e77c7bfd8e6c26
Heena 1 <heenaagrawalbbd@gmail.com>
2020-07-08 12:05:13 +0530

    second commit

--- UserServices/Startup.cs
+++ UserServices/Startup.cs
@@ -41,7 +41,7 @@ namespace UserServices
 
                 return tracer;
             });
-            services.AddOpenTracing(); ;
+            services.AddOpenTracing();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
